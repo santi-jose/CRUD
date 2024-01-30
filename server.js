46b278b39f1b4ce58321ecf3edb3aeed301a8397
jlsantia21 <jlsantia21@gmail.com>
2024-01-30 02:08:04 -0800

    Completed CRUD Prisma assignment. Finished README detailing final changes and implementation of Prisma schema to CRUD web app.

--- server.js
+++ server.js
@@ -17,59 +17,105 @@ app.use(bodyParser.json());
 
 // configure each route with directions for HTTP requests
 
-// app.post('/users', (req, res) => {
-//     const {username, password} = req.body; // get username and password from req.body
-//     prisma.user.create({
-//         data: {
-//             username,
-//             password,
-//             posts: {
-//                 create: {
-//                     title: 'My first post',
-//                     body: 'Lots of really interesting stuff',
-//                 },
-//             },
-//         }
-//     })
-//     .then(result => {
-//         res.redirect('/');
-//     })
-//     .catch(error => {
-//         // Handle errors
-//         console.error(error);
-//         res.status(500).json({error: "Internal Server Error"});
-//     });
-// });
+// post users when they are submitted through submit-button
+app.post('/users', async(req, res) => {
+    const {username, password} = req.body; // get username and password from req.body
+    prisma.user.create({ // create new user given req body params
+        data: {
+            username,
+            password,
+            posts: {
+                create: {
+                    title: `My first post, by: ${username}`,
+                    body: 'Lots of really interesting stuff',
+                },
+            },
+        }
+    })
+    .then(result => {
+        res.redirect('/');
+    })
+    .catch(error => {
+        // Handle errors
+        console.error(error);
+        res.status(500).json({error: "Internal Server Error"});
+    });
+});
+
+// post user made posts 
+app.post('/posts', async(req, res)=>{
+    const {title, body} = req.body; // get title and body from req.body    
+    prisma.post.create({ // create new post given req  body params
+        data: {
+            title,
+            body,
+            user: {
+                create: {
+                    username: 'prisma.post.create Test User',
+                    password: 'prisma.post.create Test Password',
+                },
+            },
+        }
+    })
+    .then(result => {
+        res.redirect('/');
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).json({error: "Internal Server Error"});
+    });
+});
 
 // connect to database
 MongoClient.connect(process.env.MONGO_URI)
     .then(client => { // connect to MongoClient
-        const db = client.db('practice'); // create db practice
-        const usersCollection = db.collection('users'); // create collection of users
+        const db = client.db('practice'); // connect to db practice
+        const usersCollection = db.collection('User'); // access the User collection
 
         // handle get request at root 
-        app.get('/', (req, res) => {
-            usersCollection
-                .find() // get cursor to iterate through collection
-                .toArray() // returns an array of documents of collection
-                .then(results => { // when resolved render index.ejs with results as userCollections
-                    res.render('index.ejs', {usersCollection: results});
-                }) // error handling
+        // app.get('/', (req, res) => {
+        //     usersCollection
+        //         .find() // get cursor to iterate through collection
+        //         .toArray() // returns an array of documents of collection
+        //         .then(results => { // when resolved render index.ejs with results as userCollections
+        //             res.render('index.ejs', {usersCollection: results});
+        //         }) // error handling
+        //         .catch(error => console.error(error));
+        // });
+
+        // handle get request with prisma
+        app.get('/', async(req, res) => {
+            const body = { users: null, posts: null };
+
+            const users = await prisma.user
+                .findMany()
+                .then(results => {
+                    body.users = results;
+                })
                 .catch(error => console.error(error));
-        });
+
+            const posts = await prisma.post
+                .findMany()
+                .then(results => {
+                    body.posts = results;
+                })
+                .catch(error => console.error(error));
+
+            res.render('index.ejs', {body: body});
+        })
 
         // handle post requests at 'users'
-        app.post('/users', (req, res) => {
-            usersCollection
-                .insertOne(req.body) // insert the request body into collection
-                .then(result => { // if inser successful redirect to root
-                    res.redirect('/');
-                }) // error handling
-                .catch(error => console.log(error));
-        });
+        // app.post('/users', (req, res) => {
+        //     usersCollection
+        //         .insertOne(req.body) // insert the request body into collection
+        //         .then(result => { // if inser successful redirect to root
+        //             res.redirect('/');
+        //         }) // error handling
+        //         .catch(error => console.log(error));
+        // });
 
         // handle put request at 'users'
-        app.put('/users', (req, res) => {
+        app.put('/users', async(req, res) => {
             usersCollection
                 .findOneAndUpdate(
                     { username: req.body.username },
@@ -94,7 +140,7 @@ MongoClient.connect(process.env.MONGO_URI)
         });
 
         // handle delete requests
-        app.delete('/users', (req, res) => {
+        app.delete('/users', async(req, res) => {
             usersCollection
                 .deleteOne(
                     { username: req.body.username }
